24eac558d6627128eaaa4d5702416b7411b79d6a
GitM4n <korolyov.ahmed@bk.ru>
2024-04-14 09:52:23 +0300

    r

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.GLOBAL_PORT || "http://localhost:5000";
+const URL = import.meta.env.GLOBAL_PORT || "http://localhost:5000";
 
 export const socket = io(URL, {
   autoConnect:false

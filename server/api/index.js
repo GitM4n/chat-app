89c21ab0bc1c7c915f65177eee0a3e001c4d442c
GitM4n <korolyov.ahmed@bk.ru>
2024-04-14 15:39:01 +0300

    r

--- server/api/index.js
+++
@@ -1,9 +0,0 @@
-const { app, server } = require('../index');
-
-// Этот обработчик маршрута необходим для серверных функций Vercel
-app.get('/', (req, res) => {
-    res.send('OK');
-});
-
-// Экспорт `server`, а не `app`, важен для работы Socket.IO
-module.exports = server;
